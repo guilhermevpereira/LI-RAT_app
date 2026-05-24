7db9131cf11c72e3964a0b0f9f036c8f2b244b00
Guilherme Vaz Pereira <guigavazpereira@gmail.com>
2026-05-23 22:58:35 -0300

    fix trial image paths

--- script.js
+++ script.js
@@ -40,8 +40,8 @@ const CONFIG = {
 
   practice: {
     id: 'practice',
-    visImg: 'img/demo/stone.jpg',
-    conImg: 'img/demo/stethoscope.jpg',
+    visImg: 'demo/stone.jpg',
+    conImg: 'demo/stethoscope.jpg',
     solutions: ['coração']
   },
 
